df5967849d4ca549e1bf4647fbf5357014c46206
Ramzi <admin@MacBook.local>
2023-07-10 21:27:30 +0300

    updating configurations

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"log"
+	"github.com/sirupsen/logrus"
 	"notes-rew/internal/app"
 	"notes-rew/internal/config"
 )
 
 func main() {
 
-	if err := config.Loader(); err != nil {
-		log.Fatalln(err)
-	}
-
-	newApp := app.NewApp()
-	if err := newApp.Start(config.ServerPort()); err != nil {
-		log.Fatalln(err)
+	if err := app.NewApp().Start(config.ServerPort()); err != nil {
+		logrus.Fatal(err)
 	}
 
 }
